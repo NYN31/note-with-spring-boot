79357054bfdc0422d810239a77de69ef10896a26
NYN31 <noyoncse310197@gmail.com>
2023-09-26 23:15:45 +0600

    export google login and registration api

--- src/main/java/note/share/controller/AuthController.java
+++ src/main/java/note/share/controller/AuthController.java
@@ -1,8 +1,10 @@
 package note.share.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import note.share.dto.request.GoogleLoginRequest;
 import note.share.dto.request.LoginRequest;
 import note.share.dto.request.RegistrationRequest;
+import note.share.dto.response.CommonResponse;
 import note.share.dto.response.LoginResponse;
 import note.share.dto.response.RegistrationResponse;
 import note.share.service.AuthService;
@@ -33,9 +35,21 @@ public class AuthController {
         return Util.form(authService.userLogin(loginRequest), HttpStatus.OK);
     }
 
+    @PostMapping("/google/login")
+    public ResponseEntity<LoginResponse> googleLogin(@RequestBody GoogleLoginRequest request) {
+        log.info("Received request to login google user");
+        return Util.form(authService.googleUserLogin(request), HttpStatus.OK);
+    }
+
     @PostMapping("/registration")
     public ResponseEntity<RegistrationResponse> registration(@RequestBody RegistrationRequest registrationRequest) {
         log.info("Received request to signup user");
         return Util.form(authService.userRegistration(registrationRequest), HttpStatus.OK);
+    }
+
+    @PostMapping("/google/registration")
+    public ResponseEntity<CommonResponse> googleRegistration(@RequestBody GoogleLoginRequest request) {
+        log.info("Received request to signup google user");
+        return Util.form(authService.googleUserRegistration(request), HttpStatus.OK);
     }
 }
